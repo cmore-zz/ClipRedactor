2cd3f29f483dba9ac5ad7304dead5812ee15c79c
cmore <cmore@takemobi.com>
2025-06-06 20:58:34 -0400

    Initial Commit

---
+++ ClipGuard/ClipGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipGuardApp.swift
+//  ClipGuard
+//
+//  Created by Charles Morehead on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipGuardTests/ClipGuardTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClipGuardTests.swift
+//  ClipGuardTests
+//
+//  Created by Charles Morehead on 6/6/25.
+//
+
+import Testing
+
+struct ClipGuardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
